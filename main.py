2a5e8e881ca806cc763b0deb29ee1aac61f33e03
asper104c22ug104cap037 <144329863+asper104c22ug104cap037@users.noreply.github.com>
2023-09-13 15:19:25 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,11 +1,49 @@
-def fact_rec(n):
-  if n == 0 or n == 1:
-    return 1
-  else:
-    return n * fact_rec(n - 1)
+class Player:
+
+    def play(self):
+
+        print("The player is playing cricket.")
 
 
-number = 2
-res = fact_rec(number)
 
-print("The factorial of {} is {}".format(number, res))
+
+
+class Batsman(Player):
+
+    def play(self):
+
+        print("The batsman is batting.")
+
+
+
+
+
+class Bowler(Player):
+
+    def play(self):
+
+        print("The bowler is bowling.")
+
+
+
+
+
+# Testing the Player, Batsman, and Bowler classes
+
+if __name__ == "__main__":
+
+    player = Player()
+
+    player.play()  # Output: The player is playing cricket.
+
+
+
+    batsman = Batsman()
+
+    batsman.play()  # Output: The batsman is batting.
+
+
+
+    bowler = Bowler()
+
+    bowler.play()  # Output: The bowler is bowling.
